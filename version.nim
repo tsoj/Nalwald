0819989726fca25787b5ae78be4a0ac8bf9c36c9
tsoj <tsoj.tsoj@gmx.de>
2022-02-08 01:55:11 +0100

    Version 15

--- version.nim
+++ version.nim
@@ -1,5 +1,5 @@
 import strutils
 
-func version*(): string = "14.TODO"
+func version*(): string = "15"
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
 func compileYear*(): string = CompileDate.split('-')[0]
