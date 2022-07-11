bf4ea6db6d5e01caa5d5b2afd92d397763106bdf
tsoj <tsoj.tsoj@gmx.de>
2022-07-11 23:42:14 +0200

    Nalwald 16

--- version.nim
+++ version.nim
@@ -1,5 +1,5 @@
 import strutils
 
-func version*(): string = "15.TODO"
+func version*(): string = "16"
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
 func compileYear*(): string = CompileDate.split('-')[0]
