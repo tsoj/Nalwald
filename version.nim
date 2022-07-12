340cf8e20aa18a129c7f1c5c2060ec22fec0d539
tsoj <tsoj.tsoj@gmx.de>
2022-07-12 09:26:28 +0200

    New development version, -1 +/-3 vs new pool

--- version.nim
+++ version.nim
@@ -1,5 +1,5 @@
 import strutils
 
-func version*(): string = "16"
+func version*(): string = "16.TODO"
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
 func compileYear*(): string = CompileDate.split('-')[0]
