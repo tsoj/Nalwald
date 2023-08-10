46d34c5bbc5e7e31fcb4d89fb152e6ac99f7a251
tsoj <tsoj.tsoj@gmx.de>
2023-08-11 01:45:57 +0200

    Added support for non-git repo compile

--- version.nim
+++ version.nim
@@ -5,31 +5,44 @@ import std/[
 
 export options
 
-const
-    gitHasUnstagedChanges = "nothing to commit, working tree clean" notin staticExec("git status")
-    gitHash = staticExec("git rev-parse HEAD").strip
-    gitShortHash = staticExec("git rev-parse --short HEAD").strip
-    gitTag = staticExec("git tag --points-at HEAD").strip
-
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
-
 func compileYear*(): string = CompileDate.split('-')[0]
 
-func version*(): Option[string] =
-    when not gitTag.isEmptyOrWhitespace and not gitHasUnstagedChanges:
-        some(gitTag)
+when "git version" notin staticExec("git -v") or "not a git repository" in staticExec("git status"):
+
+    func version*(): Option[string] =
+        none(string)
     
-func id(): string =
-    result = gitShortHash
-    when gitHasUnstagedChanges:
+    func id(): string =
         const d = CompileDate.split('-')
         const t = CompileTime.split(':')
-        result &= "-" & d[0] & d[1] & d[2] & t[0] & t[1] & t[2]
+        "unspecified-" & d[0] & d[1] & d[2] & t[0] & t[1] & t[2]
+
+    func commitHash*(): string =
+        "unspecified"
+else:
+    
+    const
+        gitHasUnstagedChanges = "nothing to commit, working tree clean" notin staticExec("git status")
+        gitHash = staticExec("git rev-parse HEAD").strip
+        gitShortHash = staticExec("git rev-parse --short HEAD").strip
+        gitTag = staticExec("git tag --points-at HEAD").strip
+
+    func version*(): Option[string] =
+        when not gitTag.isEmptyOrWhitespace and not gitHasUnstagedChanges:
+            some(gitTag)
+        
+    func id(): string =
+        result = gitShortHash
+        when gitHasUnstagedChanges:
+            const d = CompileDate.split('-')
+            const t = CompileTime.split(':')
+            result &= "-" & d[0] & d[1] & d[2] & t[0] & t[1] & t[2]
+
+    func commitHash*(): string =
+        result = gitHash
+        when gitHasUnstagedChanges:
+            result &= " + unstaged changes"
 
 func versionOrId*(): string =
     version().get(otherwise = id())
-
-func commitHash*(): string =
-    result = gitHash
-    when gitHasUnstagedChanges:
-        result &= " + unstaged changes"
