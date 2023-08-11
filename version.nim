4fe6852e0de3040adf35fd9e33645a42c26d9970
tsoj <tsoj.tsoj@gmx.de>
2023-08-11 02:09:24 +0200

    Changed git -v to git --version

--- version.nim
+++ version.nim
@@ -8,11 +8,11 @@ export options
 func compileDate*(): string = CompileDate & " " & CompileTime & " (UTC)"
 func compileYear*(): string = CompileDate.split('-')[0]
 
-when "git version" notin staticExec("git -v") or "not a git repository" in staticExec("git status"):
+when "git version" notin staticExec("git --version") or "not a git repository" in staticExec("git status"):
 
     static:
         debugEcho "WARNING: Git not available or not a git repo"
-        debugEcho "git -v: ", staticExec("git -v"), "\ngit status: ", staticExec("git status")
+        debugEcho "git --version: ", staticExec("git --version"), "\ngit status: ", staticExec("git status")
 
     func version*(): Option[string] =
         none(string)
