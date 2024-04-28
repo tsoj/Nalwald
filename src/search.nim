9fe5c754d15b3a9fcb9076069f408b39a0013fc7
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-04-28 03:29:55 +0200

    Testing bench

--- src/search.nim
+++ src/search.nim
@@ -178,7 +178,7 @@ func search(
     let beta = block:
         # update alpha, beta or return immediatly based on hash table result
         var beta = beta
-        if height > 3 and not hashResult.isEmpty:
+        if height > 0 and not hashResult.isEmpty:
             if hashResult.nodeType == exact and hashResult.depth >= depth:
                 return hashResult.value
 
