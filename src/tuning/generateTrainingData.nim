71ac3607782241335c315b45bf98a5e40190464e
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-07-06 23:29:24 +0200

    renamed folder

--- src/tuning/generateTrainingData.nim
+++ src/tuning/generateTrainingData.nim
@@ -40,7 +40,7 @@ doAssert not gitHasUnstagedChanges,
 
 let
   startDate = now().format("yyyy-MM-dd-HH-mm-ss")
-  outDir = "res/trainingSetsXXX/"
+  outDir = "res/trainingSets/"
   outputFilename =
     fmt"{outDir}trainingSet_{startDate}_{sampleGameSearchNodes}_{versionOrId()}.bin"
 
