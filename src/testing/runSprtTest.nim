6979f799acac446a994375477bdf5629606f3b01
Jost Triller <tsoj.tsoj@gmx.de>
2024-12-29 23:24:18 +0100

    SPRT log

--- src/testing/runSprtTest.nim
+++ src/testing/runSprtTest.nim
@@ -65,6 +65,7 @@ createDir pgnOutDir
 let cuteChessArguments =
   fmt""" \
 -config discard=true outname={workDir}/config.json \
+-log file={workDir}/log.log level=warn compress=true \
 -concurrency {max(1, countProcessors() - 2)} \
 -ratinginterval 50 \
 -games 2 -rounds {maxNumGames} \
