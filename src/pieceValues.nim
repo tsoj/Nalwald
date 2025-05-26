1f8979fdfa977bc6e019111ce8d4580d880c2814
Jost Triller <tsoj.tsoj@gmx.de>
2025-05-27 01:31:46 +0200

    Clean slate

--- src/pieceValues.nim
+++
@@ -1,5 +0,0 @@
-import evalParameters
-
-func value*(piece: Piece): Value =
-  const table = [pawn: 145.Value, knight: 471.Value, bishop: 511.Value, rook: 740.Value, queen: 1488.Value, king: valueCheckmate, noPiece: 0.Value]
-  table[piece]
