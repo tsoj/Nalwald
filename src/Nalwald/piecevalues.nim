b39db58b63d6e0ed3b71688642c7cf475a909d79
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-06 23:34:04 +0200

    Trained king relative psqts
    Bench: 7947550

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -3,5 +3,5 @@ import nimchess
 import types
 
 func value*(piece: Piece): Value =
-  const table = [pawn: 0.31304058.Value, knight: 0.95035917.Value, bishop: 1.0092459.Value, rook: 1.5511856.Value, queen: 3.290629.Value, king: valueCheckmate, noPiece: 0.Value]
+  const table = [pawn: 0.30897307.Value, knight: 0.8749822.Value, bishop: 0.9424647.Value, rook: 1.4613937.Value, queen: 2.7174075.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
