28003443d5f2f4ed3cb7e98af1aa9e5fc03a50f4
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-06 17:43:21 +0200

    Trained on newer and more data Bench: 12241880

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -3,13 +3,5 @@ import nimchess
 import types
 
 func value*(piece: Piece): Value =
-  const table = [
-    pawn: 0.27807832.Value,
-    knight: 0.86215657.Value,
-    bishop: 0.88966936.Value,
-    rook: 1.3782328.Value,
-    queen: 2.9001288.Value,
-    king: valueCheckmate,
-    noPiece: 0.Value,
-  ]
+  const table = [pawn: 0.3121768.Value, knight: 0.95103383.Value, bishop: 1.0107707.Value, rook: 1.553279.Value, queen: 3.2977438.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
