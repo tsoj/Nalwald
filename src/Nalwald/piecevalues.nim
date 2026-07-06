35ca473885f92a468e5dd7874a75a5f8a345d2f4
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-06 15:52:30 +0200

    Only wdl phased psqts Bench: 11956433

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -3,5 +3,5 @@ import nimchess
 import types
 
 func value*(piece: Piece): Value =
-  const table = [pawn: 0.27807832.Value, knight: 0.86215657.Value, bishop: 0.88966936.Value, rook: 1.3782328.Value, queen: 2.9001288.Value, king: valueCheckmate, noPiece: 0.Value]
+  const table = [pawn: 0.33712408.Value, knight: 1.1426086.Value, bishop: 1.2008091.Value, rook: 1.8742312.Value, queen: 6.900756.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
