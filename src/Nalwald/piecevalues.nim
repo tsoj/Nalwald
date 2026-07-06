9a4722a74100f54bf0381e9bee3d1609db4ac063
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-06 17:24:52 +0200

    back to 0.5 wdl score

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -3,5 +3,5 @@ import nimchess
 import types
 
 func value*(piece: Piece): Value =
-  const table = [pawn: 0.24152832.Value, knight: 0.6872378.Value, bishop: 0.6947635.Value, rook: 1.1474837.Value, queen: 2.1260507.Value, king: valueCheckmate, noPiece: 0.Value]
+  const table = [pawn: 0.27807832.Value, knight: 0.86215657.Value, bishop: 0.88966936.Value, rook: 1.3782328.Value, queen: 2.9001288.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
