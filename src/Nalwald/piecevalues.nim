0d5b5a9ce67b0545c75859de06544d99771d1907
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-07 13:27:07 +0200

    Trained with mate score positions
    Bench: 3326217

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -4,6 +4,6 @@ import types
 
 #!fmt: off
 func value*(piece: Piece): Value =
-  const table = [pawn: 0.32239842.Value, knight: 1.0803405.Value, bishop: 1.1097395.Value, rook: 1.6884761.Value, queen: 3.6470497.Value, king: valueCheckmate, noPiece: 0.Value]
+  const table = [pawn: 0.3221577.Value, knight: 1.0777335.Value, bishop: 1.1085387.Value, rook: 1.6835519.Value, queen: 3.6307063.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
 #!fmt: on
