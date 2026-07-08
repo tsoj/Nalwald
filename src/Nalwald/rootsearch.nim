56a7abcab8ca8c0a97801fa99c98f892a72dd3c7
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-08 21:03:00 +0200

    Added aspiration windows
    Bench: 15471839

--- src/Nalwald/rootsearch.nim
+++ src/Nalwald/rootsearch.nim
@@ -116,26 +116,22 @@ func alphabeta(
 
     let givingCheck = newPosition.inCheck(newPosition.us)
 
-    var
-      newDepth = depth
-      newBeta = beta
+    var newDepth = depth
 
     if not givingCheck and moveCounter >= 4 and not move.isTactical:
       newDepth = lmrDepth(newDepth, lmrMoveCounter)
       lmrMoveCounter += 1
-      if alpha.abs < queen.value:
-        newBeta = min(beta, alpha + pawn.value * 0.1)
 
     var value = -newPosition.alphabeta(
       state,
-      alpha = -newBeta,
+      alpha = -beta,
       beta = -alpha,
       depth = newDepth - 1.Effort,
       height = height + 1,
     )
 
     # re-search with full window and full depth
-    if value > alpha and (newDepth < depth or newBeta < beta):
+    if value > alpha and newDepth < depth:
       newDepth = depth
       value = -newPosition.alphabeta(
         state,
@@ -176,6 +172,34 @@ func alphabeta(
 
   return bestValue
 
+func alphabetaRoot*(
+    position: SearchPos, state: var SearchState, depth: Effort, previousValue: Value
+): Value =
+  result = 0
+
+  var
+    estimatedValue = previousValue
+    alphaOffset = 0.05 * pawn.value
+    betaOffset = 0.05 * pawn.value
+
+  # growing alpha beta window
+  while not state.shouldStop:
+    let
+      alpha = max(estimatedValue - alphaOffset, -Inf).Value
+      beta = min(estimatedValue + betaOffset, Inf).Value
+
+    result =
+      position.alphabeta(state, alpha = alpha, beta = beta, depth = depth, height = 0)
+    doAssert result.abs <= Inf
+
+    estimatedValue = result
+    if result <= alpha:
+      alphaOffset *= 3.0
+    elif result >= beta:
+      betaOffset *= 3.0
+    else:
+      break
+
 proc search*(
     params: GoParams,
     hashTable: var HashTable,
@@ -214,7 +238,7 @@ proc search*(
     let prevNodes = state.countedNodes.float
 
     let bestValue =
-      position.alphabeta(state, alpha = -Inf, beta = Inf, depth = depth, height = 0)
+      position.alphabetaRoot(state, depth = depth, previousValue = finalValue)
 
     let
       currNodes = state.countedNodes.float
