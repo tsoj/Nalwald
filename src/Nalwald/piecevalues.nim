d3dc5cd46c339469a07f80338d15531e710ba638
Jost Triller <tsoj.tsoj@gmx.de>
2026-07-09 17:45:51 +0200

    Trained pawn psqts
    Bench: 9690348

--- src/Nalwald/piecevalues.nim
+++ src/Nalwald/piecevalues.nim
@@ -4,6 +4,6 @@ import types
 
 #!fmt: off
 func value*(piece: Piece): Value =
-  const table = [pawn: 0.40252742.Value, knight: 1.2913749.Value, bishop: 1.3843408.Value, rook: 2.0380604.Value, queen: 4.4362316.Value, king: valueCheckmate, noPiece: 0.Value]
+  const table = [pawn: 0.42250013.Value, knight: 1.3164449.Value, bishop: 1.4307503.Value, rook: 2.1102395.Value, queen: 4.591611.Value, king: valueCheckmate, noPiece: 0.Value]
   table[piece]
 #!fmt: on
